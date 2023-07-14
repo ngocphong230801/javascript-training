c1fac45b061be3a42c3a39a7e389997aaa991015
ngocphong230801 <nnphong962015@gmail.com>
2023-07-14 09:12:19 +0700

    Init examples

--- examples/src/js/form.js
+++ examples/src/js/form.js
@@ -1,10 +1,10 @@
 const form = document.getElementById('registration-form');
 const fullname = document.getElementById('fullname');
 const email = document.getElementById('email');
+const address = document.getElementById('address');
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirm-password');
 const memberTable = document.getElementById('member-table');
-
 
 form.addEventListener('submit', function (event) {
   event.preventDefault();
@@ -13,6 +13,9 @@ form.addEventListener('submit', function (event) {
   if (fullname.value.trim() === '') {
     document.getElementById('fullname-error').textContent = 'Please enter your fullname';
     isValid = false;
+    console.log('Please enter your name');
+  } else if (!isNaN(fullname.value.trim())) {
+    document.getElementById('fullname-error').textContent = 'Not Number';
   } else {
     document.getElementById('fullname-error').textContent = '';
   }
@@ -34,6 +37,14 @@ form.addEventListener('submit', function (event) {
   else {
     document.getElementById('password-error').textContent = '';
   }
+  // Check address
+  if (address.value.trim() === '') {
+    document.getElementById('address-error').textContent = 'Please enter your address'
+    isValid = false;
+  } else {
+    document.getElementById('address-error').textContent = '';
+  }
+
   // Check confim password
   if (confirmPassword.value.trim() === '') {
     document.getElementById('confirm-password-error').textContent = 'Please confim the password';
@@ -47,13 +58,20 @@ form.addEventListener('submit', function (event) {
   }
   if (isValid) {
     // Add values to table
+
+    localStorage.getItem('listAcount')
+    localStorage.setItem('listAcount', JSON.stringify(list))
+
     const row = memberTable.insertRow(-1);
     const fullNameCell = row.insertCell(0);
     const emailCell = row.insertCell(1);
+    const addressCell = row.insertCell(2)
 
     fullNameCell.textContent = fullname.value.trim();
     emailCell.textContent = email.value.trim();
+    addressCell.textContent = address.value.trim();
     // Reset form
     form.reset();
   }
 });
+
--- examples/src/js/loop.js
+++ examples/src/js/loop.js
@@ -1,3 +1,31 @@
 for (let i = 1; i<=100; i++) {
     console.log(i);
 }
+
+const myArray = [
+    'Js',
+    'HTML',
+    'CSS',
+    'Python'
+]
+
+const arraylength = myArray.length
+
+for(let i=0;i< arraylength;i++){
+    console.log(myArray[i])
+}
+
+const Countries = [
+    'Viet Nam',
+    'England',
+    'France',
+    'Laos',
+    'Italy'
+]
+
+for (let key in Countries) {
+    console.log(Countries[key])
+};
+
+
+
