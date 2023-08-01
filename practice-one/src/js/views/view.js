9448de27c684774f05b4863fead3ee963b857680
ngocphong230801 <nnphong962015@gmail.com>
2023-08-01 10:12:57 +0700

    Update to comments

--- practice-one/src/js/views/view.js
+++ practice-one/src/js/views/view.js
@@ -6,96 +6,114 @@ import imgDelete from "../../assets/icon/delete.png";
 import imgDetail from "../../assets/icon/detail.jpg";
 
 class BookView {
-  constructor() {
-    this.bookListElement = getId("list-books");
-    this.validationForm = getId("validation-form");
-    this.overlay = getId("overlay");
+    constructor() {
+        this.bookListElement = getId("list-books");
+        this.validationForm = getId("validation-form");
+        this.overlay = getId("overlay");
 
-    getId("create").addEventListener("click", this.showValidationForm);
-    getId("cancel").addEventListener("click", this.hideValidationForm);
-    getId("save").addEventListener("click", this.handleSaveButtonClick);
-    this.overlay.addEventListener("click", this.hideValidationForm.bind(this));
+        document.querySelector(".create").addEventListener("click", this.showValidationForm);
+        document.querySelector(".cancel").addEventListener("click", this.hideValidationForm);
+        document.querySelector(".save").addEventListener("click", this.handleSaveButtonClick);
+        this.overlay.addEventListener("click", this.hideValidationForm.bind(this));
 
-    this.init();
-  }
-
-  init = () => {
-    this.hideValidationForm();
-    this.loadSavedBooks();
-  };
-
-  showValidationForm = () => {
-    this.setDisplay("block");
-    this.clearErrorMessages();
-    this.validationForm.reset();
-  };
-
-  hideValidationForm = () => {
-    this.setDisplay("none");
-  };
-
-  handleSaveButtonClick = (event) => {
-    event.preventDefault();
-    if (validateForm(this.validationForm)) {
-      this.saveDataToLocalStorage();
-      this.hideValidationForm();
-      this.validationForm.reset();
-      this.loadSavedBooks();
+        this.init();
     }
-  };
 
-  setDisplay = (displayValue) => {
-    this.validationForm.style.display = displayValue;
-    this.overlay.style.display = displayValue;
-  };
+    init = () => {
+        this.hideValidationForm();
+        this.loadSavedBooks();
+    };
 
-  clearErrorMessages = () => {
-    const errorElements = this.validationForm.querySelectorAll(".error");
-    errorElements.forEach((errorElement) => {
-      errorElement.textContent = "";
-    });
-  };
+    showValidationForm = () => {
+        this.setDisplay("block");
+        this.clearErrorMessages();
+        this.validationForm.reset();
+    };
 
-  saveDataToLocalStorage = () => {
-    const formInputs = this.validationForm.querySelectorAll(".form-input");
-    const dataToSave = {};
+    hideValidationForm = () => {
+        this.setDisplay("none");
+    };
 
-    formInputs.forEach((input) => {
-      const fieldName = input.getAttribute("name");
-      const fieldValue = input.value;
-      dataToSave[fieldName] = fieldValue;
-    });
+    handleSaveButtonClick = (event) => {
+        event.preventDefault();
+        if (validateForm(this.validationForm)) {
+            this.saveDataToLocalStorage();
+            this.hideValidationForm();
+            this.validationForm.reset();
+            this.loadSavedBooks();
+        }
+    };
 
-    const existingData = storage.get("savedBooks") || [];
-    existingData.push(dataToSave);
+    setDisplay = (displayValue) => {
+        this.validationForm.style.display = displayValue;
+        this.overlay.style.display = displayValue;
+    };
 
-    storage.save("savedBooks", existingData);
-  };
+    clearErrorMessages = () => {
+        const errorElements = this.validationForm.querySelectorAll(".error");
+        errorElements.forEach((errorElement) => {
+            errorElement.textContent = "";
+        });
+    };
 
-  loadSavedBooks = () => {
-    const savedBooks = storage.get("savedBooks");
-    if (savedBooks) {
-      this.displayAllBooks(savedBooks);
-    }
-  };
+    saveDataToLocalStorage = () => {
+        const formInputs = this.validationForm.querySelectorAll(".form-input");
+        const dataToSave = {};
 
-  displayAllBooks = (books) => {
-    let html = "";
-    books.forEach((bookInfo) => {
-      html += `
+        formInputs.forEach((input) => {
+            const fieldName = input.getAttribute("name");
+            const fieldValue = input.value;
+            dataToSave[fieldName] = fieldValue;
+        });
+
+        let existingData;
+        try {
+            existingData = storage.get("savedBooks") || [];
+        } catch (error) {
+            console.error("Error while retrieving savedBooks from LocalStorage:", error);
+            existingData = [];
+        }
+
+        existingData.push(dataToSave);
+
+        try {
+            storage.save("savedBooks", existingData);
+        } catch (error) {
+            console.error("Error while saving data to LocalStorage:", error);
+        }
+    };
+
+    loadSavedBooks = () => {
+        let savedBooks;
+        try {
+            savedBooks = storage.get("savedBooks");
+        } catch (error) {
+            console.error("Error while retrieving savedBooks from LocalStorage:", error);
+            savedBooks = null;
+        }
+
+        if (savedBooks) {
+            this.displayAllBooks(savedBooks);
+        }
+    };
+
+    displayAllBooks = (books) => {
+        let bookListHTMl = "";
+        books.forEach((bookInfo) => {
+            bookListHTMl += `
                 <li class = "book">
-                <h3 class = "book-title">${bookInfo.bookname}</h3>
-                <p class = "book-author">${bookInfo.author}</p>
-                <p class = "book-date">${bookInfo.date}</p>
-                <p class = "book-description">${bookInfo.description}</p>
-                <img src="${imgDetail}" alt="delete" class="detail">
-                <img src="${imgDelete}" alt="delete" class="delete">
+                    <h3 class = "book-title">${bookInfo.bookname}</h3>
+                    <p class = "book-author">${bookInfo.author}</p>
+                    <p class = "book-date">${bookInfo.date}</p>
+                    <p class = "book-description">${bookInfo.description}</p>
+                    <img src="${imgDetail}" alt="delete" class="detail">
+                    <img src="${imgDelete}" alt="delete" class="delete">
                 </li>
             `;
-    });
+        });
 
-    this.bookListElement.innerHTML = html;
-  };
+        this.bookListElement.innerHTML = bookListHTMl;
+    };
 }
 
 export default BookView;
