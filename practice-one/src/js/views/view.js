a69d2264b87b37772049696acebf1778d1dc87d8
ngocphong230801 <nnphong962015@gmail.com>
2023-08-04 15:18:42 +0700

    Change view file

--- practice-one/src/js/views/view.js
+++ practice-one/src/js/views/view.js
@@ -96,9 +96,9 @@ class BookView {
     const endIndex = startIndex + itemsPerPage;
     const booksToShow = books.slice(startIndex, endIndex);
 
-    let BookListHtml = "";
+    let bookListHtml = "";
     booksToShow.forEach((bookInfo, index) => {
-      BookListHtml += `
+      bookListHtml += `
         <li class="book" data-book-index="${startIndex + index}">
           <h3 class="book-title">${bookInfo.bookname}</h3>
           <p class="book-author">${bookInfo.author}</p>
@@ -114,7 +114,7 @@ class BookView {
       `;
     });
 
-    this.bookListElement.innerHTML = BookListHtml;
+    this.bookListElement.innerHTML = bookListHtml;
 
     const deleteButtons = document.querySelectorAll(".delete");
     deleteButtons.forEach((deleteButton) => {
