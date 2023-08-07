55dc5768f536a82c6c4ede10bcdfb584f870d86e
ngocphong230801 <nnphong962015@gmail.com>
2023-08-07 15:58:58 +0700

    Update code style guide

--- practice-one/src/js/models/model.js
+++ practice-one/src/js/models/model.js
@@ -1,35 +1,40 @@
 import storage from '../services/localStorage';
 
 class BookModel {
-  getSavedBooks = () => {
-    const savedBooks = storage.get("savedBooks");
-    return savedBooks || [];
-  };
+    getSavedBooks = () => {
+        try {
+            const savedBooks = storage.get("savedBooks");
+            return savedBooks || [];
+        } catch (error) {
+            console.error("Error while retrieving saved books:", error);
+            return [];
+        }
+    };
 
-  deleteBook = (bookIndex) => {
-    const savedBooks = storage.get("savedBooks");
-    if (savedBooks && savedBooks[bookIndex]) {
-      savedBooks.splice(bookIndex, 1);
-      storage.save("savedBooks", savedBooks);
-    }
-  };
+    deleteBook = (bookIndex) => {
+        const savedBooks = storage.get("savedBooks");
+        if (savedBooks && savedBooks[bookIndex]) {
+            savedBooks.splice(bookIndex, 1);
+            storage.save("savedBooks", savedBooks);
+        }
+    };
 
-  setCurrentPage = (currentPage) => {
-    storage.save("currentPage", currentPage);
-  };
+    setCurrentPage = (currentPage) => {
+        storage.save("currentPage", currentPage);
+    };
 
-  getFilteredBooks = (searchTerm) => {
-    const savedBooks = storage.get("savedBooks");
-    if (savedBooks) {
-      const filteredBooks = savedBooks.filter((bookInfo) => {
-        const bookName = bookInfo.bookname.toLowerCase();
-        return bookName.includes(searchTerm.toLowerCase());
-      });
-      return filteredBooks;
-    } else {
-      return [];
-    }
-  };
+    getFilteredBooks = (searchTerm) => {
+        const savedBooks = storage.get("savedBooks");
+        if (savedBooks) {
+            const filteredBooks = savedBooks.filter((bookInfo) => {
+                const bookName = bookInfo.bookname.toLowerCase();
+                return bookName.includes(searchTerm.toLowerCase());
+            });
+            return filteredBooks;
+        } else {
+            return [];
+        }
+    };
 
   deleteBookByInfo = (bookInfo) => {
     const savedBooks = storage.get("savedBooks");
