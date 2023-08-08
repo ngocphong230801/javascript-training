e4839dfde77baafcf59826562e2b838ad6243508
ngocphong230801 <nnphong962015@gmail.com>
2023-08-08 10:46:36 +0700

    Update model file

--- practice-one/src/scripts/models/book.model.js
+++ practice-one/src/scripts/models/book.model.js
@@ -1,52 +1,48 @@
 import storage from '../services/localStorage';
 
 class BookModel {
-    getSavedBooks = () => {
-        try {
-            const savedBooks = storage.get("savedBooks");
-            return savedBooks || [];
-        } catch (error) {
-            console.error("Error while retrieving saved books:", error);
-            return [];
-        }
-    };
-
-    deleteBook = (bookIndex) => {
-        const savedBooks = storage.get("savedBooks");
-        if (savedBooks && savedBooks[bookIndex]) {
-            savedBooks.splice(bookIndex, 1);
-            storage.save("savedBooks", savedBooks);
-        }
-    };
-
-    setCurrentPage = (currentPage) => {
-        storage.save("currentPage", currentPage);
-    };
-
-    getFilteredBooks = (searchTerm) => {
-        const savedBooks = storage.get("savedBooks");
-        if (savedBooks) {
-            const filteredBooks = savedBooks.filter((bookInfo) => {
-                const bookName = bookInfo.bookname.toLowerCase();
-                return bookName.includes(searchTerm.toLowerCase());
-            });
-            return filteredBooks;
-        } else {
-            return [];
-        }
-    };
-
- deleteBookByInfo = (bookId) => {
-    const savedBooks = storage.get("savedBooks");
-    if (savedBooks) {
-      const bookIndex = savedBooks.findIndex((book) => book.id === bookId);
-      if (bookIndex !== -1) {
-        savedBooks.splice(bookIndex, 1);
-        storage.save("savedBooks", savedBooks);
-      }
+  getSavedBooks = () => {
+    try {
+      const savedBooks = storage.get("savedBooks");
+      return savedBooks || [];
+    } catch (error) {
+      console.error("Error while retrieving saved books:", error);
+      return [];
     }
   };
 
+  deleteBook = (bookIndex) => {
+    const savedBooks = storage.get("savedBooks");
+    if (savedBooks && savedBooks[bookIndex]) {
+      savedBooks.splice(bookIndex, 1);
+      storage.save("savedBooks", savedBooks);
+    }
+  };
+
+  setCurrentPage = (currentPage) => {
+    storage.save("currentPage", currentPage);
+  };
+
+  getFilteredBooks = (searchTerm) => {
+    const savedBooks = storage.get("savedBooks");
+    if (savedBooks) {
+      const filteredBooks = savedBooks.filter((bookInfo) => {
+        const bookName = bookInfo.bookname.toLowerCase();
+        return bookName.includes(searchTerm.toLowerCase());
+      });
+      return filteredBooks;
+    } else {
+      return [];
+    }
+  };
+
+  deleteBookByInfo = (bookId) => {
+    const savedBooks = storage.get("savedBooks");
+    if (savedBooks) {
+      const filteredBooks = savedBooks.filter((book) => book.id !== bookId);
+      storage.save("savedBooks", filteredBooks);
+    }
+  };
   updateBookByInfo = (bookId, updatedBookInfo) => {
     const savedBooks = storage.get("savedBooks");
     if (savedBooks) {
@@ -57,17 +53,11 @@ class BookModel {
       }
     }
   };
-  
-  getBookByInfo = (bookInfo) => {
+
+  getBookById = (bookId) => {
     const savedBooks = storage.get("savedBooks");
     if (savedBooks) {
-      return savedBooks.find(
-        (book) =>
-          book.bookname === bookInfo.bookname &&
-          book.author === bookInfo.author &&
-          book.date === bookInfo.date &&
-          book.description === bookInfo.description
-      );
+      return savedBooks.find((book) => book.id === bookId);
     } else {
       return null;
     }
