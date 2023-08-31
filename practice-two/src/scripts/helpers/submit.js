2cd10c04ec9ed821757fbf540d9d5801d34bd512
ngocphong230801 <nnphong962015@gmail.com>
2023-08-31 10:34:09 +0700

    Update code style guide

--- practice-two/src/scripts/helpers/submit.js
+++ practice-two/src/scripts/helpers/submit.js
@@ -1,1 +1,5 @@
+// Define a constant variable to represent the "Enter" key when user inputs.
 export const submitInput = "Enter";
+
+// In this case, the "submitInput" constant is used to compare against the user input value
+// to determine whether they pressed the "Enter" key to submit data.
